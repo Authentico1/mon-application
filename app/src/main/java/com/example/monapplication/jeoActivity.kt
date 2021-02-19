db3b8c0f4bcfa7690f5c388311938769e1c982d0
Lionel Sokozina <lionelelauthentic@gmail.com>
2021-02-19 23:40:58 +0300

    mon application

--- app/src/main/java/com/example/monapplication/jeoActivity.kt
+++ app/src/main/java/com/example/monapplication/jeoActivity.kt
@@ -2,10 +2,17 @@ package com.example.monapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class jeoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jeo)
+
+        val buttonEnter : imageView = findViewById(R.id.imageView23)
+
+        buttonEnter.setOnClickListener{
+            Toast.makeText(applicationContext, "Message envoye", Toast.LENGTH_SHORT).show()
     }
 }
